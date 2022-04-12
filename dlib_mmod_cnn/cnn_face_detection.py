b6e66293249d4de6911b8af7a7b883dffe70ed78
Ian Lewis <ijl20@cam.ac.uk>
2022-04-12 11:35:44 +0100

    dlib_mmod_cnn working with faces.jpg

--- dlib_mmod_cnn/cnn_face_detection.py
+++ dlib_mmod_cnn/cnn_face_detection.py
@@ -1,10 +1,28 @@
 # import the necessary packages
-from pyimagesearch.helpers import convert_and_trim_bb
 import argparse
 import imutils
 import time
 import dlib
 import cv2
+
+def convert_and_trim_bb(image, rect):
+	# extract the starting and ending (x, y)-coordinates of the
+	# bounding box
+	startX = rect.left()
+	startY = rect.top()
+	endX = rect.right()
+	endY = rect.bottom()
+	# ensure the bounding box coordinates fall within the spatial
+	# dimensions of the image
+	startX = max(0, startX)
+	startY = max(0, startY)
+	endX = min(endX, image.shape[1])
+	endY = min(endY, image.shape[0])
+	# compute the width and height of the bounding box
+	w = endX - startX
+	h = endY - startY
+	# return our bounding box coordinates
+	return (startX, startY, w, h)
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -18,7 +36,7 @@ ap.add_argument("-u", "--upsample", type=int, default=1,
 args = vars(ap.parse_args())
 
 # load dlib's CNN face detector
-print("[INFO] loading CNN face detector...")
+print("[INFO] loading CNN face detector using {}...".format(args["model"]))
 detector = dlib.cnn_face_detection_model_v1(args["model"])
 # load the input image from disk, resize it, and convert it from
 # BGR to RGB channel ordering (which is what dlib expects)
@@ -30,7 +48,7 @@ start = time.time()
 print("[INFO[ performing face detection with dlib...")
 results = detector(rgb, args["upsample"])
 end = time.time()
-print("[INFO] face detection took {:.4f} seconds".format(end - start))
+print("[INFO] face detection found {} faces and took {:.4f} seconds".format(len(results), end - start))
 
 # convert the resulting dlib rectangle objects to bounding boxes,
 # then ensure the bounding boxes are all within the bounds of the
